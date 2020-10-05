443feec3f1bd432054ee549d24478b5460eb51bd
Christian Nagel <christian@christiannagel.com>
2020-10-05 16:32:57 +0200

    target typed new expression

---
+++ day1/csharp9/TargetTypedNewSample/TargetTypedNewSample/Book.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TargetTypedNewSample
+{
+    public record Book(string Title, string Publisher)
+    {
+    }
+}
---
+++ day1/csharp9/TargetTypedNewSample/TargetTypedNewSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TargetTypedNewSample
+{
+    class Program
+    {
+        private Book theBook = new("a", "b");
+
+        static void Main(string[] args)
+        {
+            var b = new Book("title", "pub");
+            Book b2 = new("title", "pub");
+
+        }
+    }
+}
