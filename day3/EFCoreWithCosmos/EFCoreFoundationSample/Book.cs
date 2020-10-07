78d91236458c522c8a88284aff7f76cdaff57221
Christian Nagel <christian@christiannagel.com>
2020-10-07 13:09:08 +0200

    EF Core with cosmos and owned entities

---
+++ day3/EFCoreWithCosmos/EFCoreFoundationSample/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EFCoreFoundationSample
+{
+    public class Book
+    {
+        public Book(string bookId, string title, string publisher)
+        {
+            BookId = bookId;
+            Title = title;
+            Publisher = publisher;
+            //if (Chapters is List<Chapter> myChapters)
+            //{
+            //    myChapters.AddRange(chapters);
+            //}
+
+        }
+        public string BookId { get; set; } = null!;
+        [StringLength(50)]
+        public string Title { get; set; } = string.Empty;
+        public string? Publisher { get; set; }
+
+        public IEnumerable<Chapter> Chapters { get; set; } = new List<Chapter>();
+    }
+}
---
+++ day3/EFCoreWithCosmos/EFCoreFoundationSample/SampleRunner.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreFoundationSample
+{
+    public class SampleRunner
+    {
+        private readonly BooksContext _booksContext;
+
+        public SampleRunner(BooksContext booksContext)
+        {
+            _booksContext = booksContext;
+        }
+
+        public async Task CreateDatabaseAsync()
+        {
+            bool created = await _booksContext.Database.EnsureCreatedAsync();
+            Console.WriteLine($"created: {created}");
+        }
+
+        public async Task CreateBooksAsync()
+        {
+            var chapters = Enumerable.Range(1, 10).Select(x => new Chapter
+            {
+                ChapterId = Guid.NewGuid().ToString(),
+                Title = $"chapter {x}"
+            }).ToArray();
+            var newBook = new Book(Guid.NewGuid().ToString(), "a new book", "Wrox Press");
+            newBook.Chapters = chapters;
+            
+            _booksContext.Books.Add(newBook);
+            int numberRecords = await _booksContext.SaveChangesAsync();
+            Console.WriteLine($"inserted {numberRecords} records");
+        }
+
+        public async Task QueryBooksAsync()
+        {
+            var asyncBooks = _booksContext.Books.Where(b => b.Publisher == "sample pub").AsAsyncEnumerable();
+            await foreach(var book in asyncBooks)
+            {
+                Console.WriteLine($"{book.Title}");
+            }
+        }
+
+    }
+}
